251e097211353a9b133e07bbc624d1151be76306
olegkondrashov <condrashov.oleg@gmail.com>
2024-12-16 09:36:15 +0100

    changed style for angebot-pdf and added arrows to sliders

--- src/scripts/slick-slider.js
+++ src/scripts/slick-slider.js
@@ -1,36 +1,6 @@
 
-$(document).ready(function(){
-    $('.slider-wrapper-leistungen').slick({
-        infinite: false,
-        slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 769,
-            settings: {
-              arrows: false,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          },
-        ]
-    });
-
-    $('.gallery-grid').slick({
-      infinite: false,
-      centerPadding: '0',
-      slidesToShow: 7,
-      responsive: [
-        {
-          breakpoint: 769,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 1
-          }
-        },
-      ]
-  });
+$(document).ready(function () {
+  //main
   $('.slider-wrapper').slick({
     infinite: false,
     centerPadding: '0',
@@ -39,49 +9,85 @@ $(document).ready(function(){
       {
         breakpoint: 769,
         settings: {
-          arrows: false,
+          arrows: true,
           centerPadding: '40px',
           slidesToShow: 1
         }
       },
     ]
-});
-$('.block').slick({
-  infinite: false,
-  slidesToShow: 4,
-  responsive: [
-    {
-      breakpoint: 769,
-      settings: {
-        arrows: false,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    },
-  ]
-});
-
-    
-    const arrowRight = document.querySelectorAll("button.slick-next.slick-arrow");
-    const arrowLeft = document.querySelectorAll("button.slick-prev.slick-arrow");
-
-    arrowLeft.forEach(arrow => {
-      // Bild als Hintergrundbild setzen
-      arrow.style.backgroundImage = "url('src/img/arrow-icon-links.png')";
-      arrow.style.backgroundRepeat = "no-repeat";
-      arrow.style.backgroundSize = "contain";
-      arrow.style.backgroundPosition = "center";
-      arrow.textContent = "";
-    })
-
-    arrowRight.forEach(arrow => {
-      // Bild als Hintergrundbild setzen
-      arrow.style.backgroundImage = "url('src/img/arrow-icon-1177.png')";
-      arrow.style.backgroundRepeat = "no-repeat";
-      arrow.style.backgroundSize = "contain";
-      arrow.style.backgroundPosition = "center";
-      arrow.textContent = "";
-    })
-
-
   });
+
+  //leistungen
+  $('.slider-wrapper-leistungen').slick({
+    infinite: false,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+    ]
+  });
+
+  //leistungen-materialien
+  $('.gallery-grid').slick({
+    infinite: false,
+    centerPadding: '0',
+    slidesToShow: 7,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+    ]
+  });
+
+  //Galerie
+  $('.block').slick({
+    infinite: false,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+    ]
+  });
+
+
+  const arrowRight = document.querySelectorAll("button.slick-next.slick-arrow");
+  const arrowLeft = document.querySelectorAll("button.slick-prev.slick-arrow");
+
+  arrowLeft.forEach(arrow => {
+    // Bild als Hintergrundbild setzen
+    arrow.style.backgroundImage = "url('src/img/arrow-icon-links.png')";
+    arrow.style.backgroundRepeat = "no-repeat";
+    arrow.style.backgroundSize = "contain";
+    arrow.style.backgroundPosition = "center";
+    arrow.textContent = "";
+  })
+
+  arrowRight.forEach(arrow => {
+    // Bild als Hintergrundbild setzen
+    arrow.style.backgroundImage = "url('src/img/arrow-icon-1177.png')";
+    arrow.style.backgroundRepeat = "no-repeat";
+    arrow.style.backgroundSize = "contain";
+    arrow.style.backgroundPosition = "center";
+    arrow.textContent = "";
+  })
+
+
+});
